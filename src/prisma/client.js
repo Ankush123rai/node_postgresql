d0c5bb736a3138e3f0851adc83acc6d60733643b
ankush123rai <84775429+Ankush123rai@users.noreply.github.com>
2024-12-24 13:35:17 +0530

    change db

--- src/prisma/client.js
+++ src/prisma/client.js
@@ -1,4 +1,15 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+async function connect() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully!");
+  } catch (error) {
+    console.error("Error connecting to the database", error);
+  }
+}
+
+connect();
 
 module.exports = prisma;
